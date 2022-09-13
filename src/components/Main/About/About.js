e68502276403b5c0b5865ce91524292758d3fb4d
Zakhar Menshikov <zm681986@mail.ru>
2022-09-13 21:24:36 +0500

    about

--- src/components/Main/About/About.js
+++ src/components/Main/About/About.js
@@ -4,19 +4,17 @@ import './About.css';
 function AboutMe() {
     return (
         <section className="about-me">
-            <h2 className="about-me__heading section__heading">Студент</h2>
             <div className="about-me__description">
-                <div className="about-me__description-text">
-                    <h3 className="about-me__description-title">Захар</h3>
-                    <p className="about-me__description-subtitle">Фронтенд-разработчик, 36 лет</p>
-                    <p className="about-me__description-paragraph">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
-                        и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы..
-                    </p>
-                    <div className="about-me__description-links">
-                        <a href="https://github.com/MenshikovZakhar" target="_blank" rel="noreferrer" className="about-me__description-link">Github</a>
-                    </div>
-                </div>
                 <img src={valeria__photo} alt="фото" className="about-me__description-photo" />
+                <div className="about-me__description-text">
+                    <h3 className="about-me__description-title">Испытай лучший</h3>
+                    <p className="about-me__description-title">Beauty сервис!</p>
+                    <p className="about-me__description-paragraph">
+                        Мы являемся ведущим салоном красоты в Челябинске, предоставляющим высококачественные парикмахерские услуги, колористикаи услуги по уходу за волосами для всех.
+
+                        Наши клиенты ценят наш уровень обслуживания, навыки наших парикмахеров и колористов, а также наш профессиональный подход ко всему, что мы делаем.
+                    </p>
+                </div>
             </div>
         </section>
     )

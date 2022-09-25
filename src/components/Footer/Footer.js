f76e534929929478b385b68602b5155807da41c3
Zakhar Menshikov <zm681986@mail.ru>
2022-09-25 15:56:07 +0500

    ABOUT

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import logo from '../../images/logo_2.png';
 import instagram from '../../images/instagram_2.svg';
 import telegram from '../../images/telegram_2.svg';
 import vk from '../../images/vk_2.svg';
-import phone from '../../images/phone_2.svg';
+import phone from '../../images/phone_2.png';
 
 function Footer() {
     return (
@@ -17,23 +17,23 @@ function Footer() {
 
                     <li className='footer__list-item'>
                         <a href="tel:+79514840077">
-                            <img className='iqons__logo' src={phone} alt='logo' />
+                            <img className='iqon__logo' src={phone} alt='logo' />
                         </a>
                     </li>
                     <li className='footer__list-item'>
                         <a href="https://instagram.com/valeria_colorist?igshid=YmMyMTA2M2Y=" target="_blank" rel="noreferrer">
-                            <img className='iqons__logo' src={instagram} alt='logo' />
+                            <img className='iqon__logo' src={instagram} alt='logo' />
                         </a>
                     </li>
 
                     <li className='footer__list-item'>
                         <a href="https://t.me/Valeria_Smolentseva" target="_blank" rel="noreferrer">
-                            <img className='iqons__logo' src={telegram} alt='logo' />
+                            <img className='iqon__logo' src={telegram} alt='logo' />
                         </a>
                     </li>
                     <li className='footer__list-item'>
                         <a href="https://vk.com/public200598040" target="_blank" rel="noreferrer">
-                            <img className='iqons__logo' src={vk} alt='logo' />
+                            <img className='iqon__logo' src={vk} alt='logo' />
                         </a>
                     </li>
 

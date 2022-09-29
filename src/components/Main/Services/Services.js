b34aef268284bee425bbea22eb88afb2ed6b4089
Zakhar Menshikov <zm681986@mail.ru>
2022-09-29 22:29:58 +0500

    Services

--- src/components/Main/Services/Services.js
+++ src/components/Main/Services/Services.js
@@ -9,13 +9,13 @@ function Services() {
         <div className='services'>
             <ul className="services__list">
                 <li className="services__link"><Link className='img_services_1' to='/services_1'> <img className='img_services' src={services} alt='' />
-                    <p>Окрашивание</p></Link></li>
-                <li className="service__link"><Link className='img_services_1' to='/services_2'> <img className='img_services' src={services} alt='' />
-                    <p>Тонирование</p></Link></li>
-                <li className="service__link"><Link className='img_services_1' to='/services_3'><img className='img_services' src={services} alt='' />
-                    <p>Женские стрижки</p></Link></li>
-                <li className="service__link"><Link className='img_services_1' to='/services_4'> <img className='img_services' src={services} alt='' />
-                    <p>Профессиональный уход</p></Link></li>
+                    <p className="services__name">Окрашивание</p></Link></li>
+                <li className="services__link"><Link className='img_services_1' to='/services_2'> <img className='img_services' src={services} alt='' />
+                    <p className="services__name">Тонирование</p></Link></li>
+                <li className="services__link"><Link className='img_services_1' to='/services_3'><img className='img_services' src={services} alt='' />
+                    <p className="services__name">Женские стрижки</p></Link></li>
+                <li className="services__link"><Link className='img_services_1' to='/services_4'> <img className='img_services' src={services} alt='' />
+                    <p className="services__name">Профессиональный уход</p></Link></li>
             </ul>
 
         </div>

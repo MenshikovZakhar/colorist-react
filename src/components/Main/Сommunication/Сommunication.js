2787b591e62145b0f738423062591eab8035bb2e
Zakhar Menshikov <zm681986@mail.ru>
2022-10-12 23:56:06 +0500

    slider

--- src/components/Main/Сommunication/Сommunication.js
+++ src/components/Main/Сommunication/Сommunication.js
@@ -4,28 +4,34 @@ import telegram from '../../../images/telegram.svg';
 import vk from '../../../images/vk.svg';
 import phone from '../../../images/phone.svg';
 import whatsapp from '../../../images/whatsapp.svg';
+import location from '../../../images/location.png';
+import email from '../../../images/email.png';
 
 function Сommunication() {
     return (
         <section className='communication'>
             <div className='communication__description'>
-                <h2 class="communication__title">По вопросам стоимости и записи обращайтесь</h2>
 
-                <div className='iqons'>
+                <div className='communication-title'>
+                    <h2 className="communication__title">По вопросам стоимости и записи обращайтесь</h2>
+                </div>
+                <p className="communication__phone">Тел: +7(951)484-00-77</p>
+
+                <div className='iqons__communication'>
                     <a href="tel:+79514840077">
-                        <img className='iqons__logo' src={phone} alt='logo' />
+                        <img className='communication__logo' src={phone} alt='logo' />
                     </a>
                     <a href="https://wa.me/79514840077" target="_blank" rel="noreferrer">
-                        <img className='iqons__logo' src={whatsapp} alt='logo' />
+                        <img className='communication__logo' src={whatsapp} alt='logo' />
                     </a>
                     <a href="https://instagram.com/valeria_colorist?igshid=YmMyMTA2M2Y=" target="_blank" rel="noreferrer">
-                        <img className='iqons__logo' src={instagram} alt='logo' />
+                        <img className='communication__logo' src={instagram} alt='logo' />
                     </a>
                     <a href="https://t.me/Valeria_Smolentseva" target="_blank" rel="noreferrer">
-                        <img className='iqons__logo' src={telegram} alt='logo' />
+                        <img className='communication__logo' src={telegram} alt='logo' />
                     </a>
                     <a href="https://vk.com/public200598040" target="_blank" rel="noreferrer">
-                        <img className='iqons__logo' src={vk} alt='logo' />
+                        <img className='communication__logo' src={vk} alt='logo' />
                     </a>
                 </div>
             </div>

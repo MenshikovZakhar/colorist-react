2e4a6a8b8e62978b039e4303a036eeed8f086902
Zakhar Menshikov <zm681986@mail.ru>
2022-10-18 00:46:57 +0500

    slider

--- src/components/Contacts/Map/Map.js
+++ src/components/Contacts/Map/Map.js
@@ -1,10 +1,10 @@
 import { YMaps, Map } from '@pbe/react-yandex-maps';
-
+import './Map.css';
 const Maps = () => (
   <YMaps>
     <div>
       йййййййй!
-      <Map defaultState={{ center: [55.145468, 61.372631], zoom: 17 }} />
+      <Map className='maps' defaultState={{ center: [55.145468, 61.372631], zoom: 17 }} />
     </div>
   </YMaps>
 );

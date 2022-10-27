2cdab270f2fe73bb8e0a2daba74dfc6f07c5b04a
Zakhar Menshikov <zm681986@mail.ru>
2022-10-27 21:11:21 +0500

    Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,10 +1,18 @@
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-
+import PortfolioCardList from '../PortfolioCardList/PortfolioCardList';
+import { arrayMovie } from '../../constants/arrayMovie';
 function Portfolio() {
     return (
         <>
             <Header />
+            <main className='movies'>
+
+                <>
+                    <PortfolioCardList arrayMovie={arrayMovie} type={'all'} />
+
+                </>
+            </main>
             <Footer />
         </>
     );
---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+// Ширина экрана
+export const WIDTH_1279 = 1279;
+export const WIDTH_767 = 767;
+
+// Количество карточек на экране
+export const NUMBER_OF_CARDS_OVER_1279 = 4;
+export const NUMBER_OF_CARDS_OVER_767 = 3;
+export const NUMBER_OF_CARDS_LESS_767 = 3;
+export const ADD_NUMBER_CARD_3 = 1;
+export const ADD_NUMBER_CARD_2 = 1;

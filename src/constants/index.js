9c121a935996847a85342f6fbebfd1e53b93518e
Zakhar Menshikov <zm681986@mail.ru>
2022-11-01 01:18:55 +0500

    Services

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,7 @@ export const WIDTH_767 = 767;
 // Количество карточек на экране
 export const NUMBER_OF_CARDS_OVER_1279 = 8;
 export const NUMBER_OF_CARDS_OVER_767 = 6;
-export const NUMBER_OF_CARDS_LESS_767 = 4;
+export const NUMBER_OF_CARDS_LESS_767 = 6;
 export const ADD_NUMBER_CARD_3 = 4;
 export const ADD_NUMBER_CARD_2 = 3;
 export const ADD_NUMBER_CARD_1 = 2;

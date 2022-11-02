214d909f1a811bf2a3d10f8ff297888bbf64bc9b
Zakhar Menshikov <zm681986@mail.ru>
2022-11-02 21:34:36 +0500

    Services

--- src/components/PortfolioCardList/PortfolioCardList.js
+++ src/components/PortfolioCardList/PortfolioCardList.js
@@ -41,9 +41,14 @@ function PortfolioCardList({ arrayPortfolio, type, onCardClick }) {
         window.addEventListener('resize', (e) =>
             setTimeOut(e.currentTarget.innerWidth)
         );
-
+        return window.removeEventListener('resize', (e) =>
+            setTimeOut(e.currentTarget.innerWidth)
+        );
     }, []);
 
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
 
     return (
         <section className='portfolio-card'>

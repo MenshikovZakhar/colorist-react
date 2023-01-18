a8a93c7c15a3a46fa74febcac29ba142493cacf9
Zakhar Menshikov <zm681986@mail.ru>
2023-01-18 21:29:28 +0500

    main

--- src/components/Сertificates/Сertificates.js
+++ src/components/Сertificates/Сertificates.js
@@ -13,7 +13,7 @@ import slide7 from "../../images/certificates__img-7.jpg"
 import slide8 from "../../images/certificates__img-8.jpg"
 import slide9 from "../../images/certificates__img-9.jpg"
 import slide10 from "../../images/certificates__img-10.jpg"
-
+const handleDragStart = (e) => e.preventDefault();
 
 const Сertificates = ({ onCardClick }) => {
     return (
